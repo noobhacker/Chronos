f203d31e78b2b1bde671652cf7e3d4cef3a6f0b7
noobhacker <leesong1995@gmail.com>
2016-03-22 21:09:19 +0800

    added event model

---
+++ ChronosWebAPI/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronosWebAPI.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+    }
+}
