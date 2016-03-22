a8542888257dca24e974e3a32ca0218173d5df89
leesong <leesong@mysoftinn.com>
2016-03-22 09:08:11 +0800

    fixed missing files

---
+++ ChronosWebAPI/Models/Confession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChronosWebAPI.Models
+{
+    public class Confession
+    {
+        public int Id { get; set; }
+        public DateTime PostDateTime { get; set; }
+        public Student PostedBy { get; set; }
+    }
+}
